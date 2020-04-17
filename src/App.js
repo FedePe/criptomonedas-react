482a314c44147cbb9fa52ddc06d2d54c216490f2
Fede <federico.andres.perez@fing.edu.uy>
2020-04-16 21:03:44 -0300

    Mostrando el resultado en la UI

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, {useState,useEffect} from 'react';
 import styled from '@emotion/styled';
 import axios from 'axios';
 import imagen from './cryptomonedas.png';
-import Formulario from './components/Formulario'
+import Formulario from './components/Formulario';
+import Cotizacion from './components/Cotizacion';
 
 const Contenedor = styled.div`
 max-width: 900px;
@@ -42,6 +43,7 @@ function App() {
 
   const [moneda, guardarMoneda] = useState('');
   const [criptomoneda, guardarCriptomoneda] = useState('');
+  const [resultado, guardarResultado] = useState({});
   
   useEffect(() => {
 
@@ -53,7 +55,7 @@ function App() {
       const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
       const resultado = await axios.get(url);
 
-      console.log(resultado.data.DISPLAY[criptomoneda][moneda]);
+      guardarResultado(resultado.data.DISPLAY[criptomoneda][moneda]);
     }
     cotizarCriptomoneda();
   }, [moneda, criptomoneda]);
@@ -72,6 +74,10 @@ function App() {
         <Formulario 
           guardarMoneda={guardarMoneda}
           guardarCriptomoneda={guardarCriptomoneda}        
+        />
+
+        <Cotizacion 
+          resultado={resultado}
         />
       </div>
     </Contenedor>
